c909fbebf0b297bd733cd4eccccdfe298765e7c8
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-08-23 13:51:14 -0300

    refactor: ft_str union

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 21:55:07 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/12 00:34:24 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/08/23 13:29:25 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,89 @@ char	*ft_strdup(const char *s)
 	}
 	dup[i] = '\0';
 	return (dup);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	ls1;
+	size_t	ls2;
+	char	*s;
+	size_t	i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	ls1 = ft_strlen(s1);
+	ls2 = ft_strlen(s2);
+	s = (char *) malloc(sizeof(char) * (ls1 + ls2 + 1));
+	if (s == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < ls1)
+		s[i] = s1[i];
+	i = -1;
+	while (++i < ls2)
+		s[i + ls1] = s2[i];
+	s[i + ls1] = '\0';
+	return (s);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len_dst;
+	size_t	len_src;
+	size_t	i;
+
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (size <= len_dst)
+		return (size + len_src);
+	i = 0;
+	while (src[i] != '\0' && i < size - len_dst - 1)
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	dst[len_dst + i] = '\0';
+	return (len_dst + len_src);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len_src;
+	size_t	i;
+
+	len_src = ft_strlen(src);
+	if (size > 0)
+	{
+		i = 0;
+		while (i < size - 1 && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (len_src);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	slen;
+	size_t	size;
+
+	if (s == NULL)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start > slen)
+		return (ft_strdup(""));
+	if (slen - start > len)
+		size = len + 1;
+	else
+		size = slen - start + 1;
+	sub = (char *) malloc(sizeof(char) * size);
+	if (sub == NULL)
+		return (NULL);
+	ft_strlcpy(sub, (s + start), size);
+	return (sub);
 }
